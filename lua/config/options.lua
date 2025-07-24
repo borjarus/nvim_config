1052702b3d6b3866359e02da53727e93af19b3e0
Miroslaw Puczynski <public.mila@gmail.com>
2025-07-24 12:19:50 +0200

    Adds initial Neovim configuration
    Sets up the initial Neovim configuration by adding several plugins
    and configuration files.
    
    This includes:
    
    - Core options configuration
    - Catppuccin theme
    - Harpoon for quick file navigation
    - LSP configuration for language support
    - One-Linters for code quality
    - Telescope for fuzzy finding
    - Treesitter for syntax highlighting
    
    These plugins and configurations aim to provide a modern and efficient
    Neovim development environment.

---
+++ lua/config/options.lua
@@ -0,0 +1,36 @@
+-- OPTIONS
+local set = vim.opt
+
+--line nums
+set.relativenumber = true
+set.number = true
+
+-- indentation and tabs
+set.tabstop = 2
+set.shiftwidth = 2
+set.autoindent = true
+set.expandtab = true
+
+-- search settings
+set.ignorecase = true
+set.smartcase = true
+
+-- appearance
+set.termguicolors = true
+set.signcolumn = "yes"
+
+-- cursor line
+set.cursorline = true
+
+-- 100th column
+set.colorcolumn = "100"
+
+-- keep cursor at least 8 rows from top/bot
+set.scrolloff = 8
+
+-- incremental search
+set.incsearch = true
+
+-- faster cursor hold
+set.updatetime = 50
+
