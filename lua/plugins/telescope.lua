201230c489659c598cfabc22e669ff04403f7686
Mirosław Puczyński <miroslaw.puczynski@currenda.pl>
2025-07-31 12:43:20 +0200

    Adds base configurations for plugins
    Adds base configurations for autopairs, comment, and lualine plugins.
    
    Updates telescope mappings for find and git files.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,8 +17,8 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+        vim.keymap.set('n', '<leader>ft', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
         vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
